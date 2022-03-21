6e8f40a0d20d1fd71ca30ef1f537bcde5aae610b
Beat Besmer <beat.besmer@gmail.com>
2022-03-21 22:55:54 +0100

    Update the waiting script

--- pi/wait.py
+++ pi/wait.py
@@ -4,10 +4,16 @@ import subprocess
 
 button = Button(21)
 
+
+subprocess.call(['sudo', 'bin/show_waiting.sh'])
+
+
 def pressed():
     print("pressed")
-    subprocess.call(['sudo', '/home/pi/ISSButton/bin/show_hero.sh'])
-    
+    subprocess.call(['sudo', 'bin/show_loading.sh'])
+    subprocess.call(['omxplayer', 'assets/SM_DE.mp4'])
+    subprocess.call(['sudo', 'bin/show_waiting.sh'])
+
 button.when_pressed = pressed
 
 pause()
