c10a91af7ea636ec95b1c3ecb5ba864e4df95ed5
Beat Besmer <beat.besmer@gmail.com>
2022-03-24 11:12:48 +0100

    Update setup script

--- pi/bin/setup.sh
+++ pi/bin/setup.sh
@@ -3,3 +3,7 @@ sudo systemctl daemon-reload
 sudo systemctl enable video-button
 sudo systemctl start video-button
 
+sudo cp etc/software-update.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable software-update
+sudo systemctl start software-update
